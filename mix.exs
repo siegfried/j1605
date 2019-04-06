d070317f9a8b4e48d7da0d64f2b86d86b8841630
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2019-04-07 02:22:59 +0800

    Add package info

--- mix.exs
+++ mix.exs
@@ -5,10 +5,14 @@ defmodule J1605.Mixfile do
     [
       app: :j1605,
       version: "0.1.0",
-      elixir: "~> 1.3",
+      elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "J1605",
+      source_url: "https://github.com/siegfried/j1605"
     ]
   end
 
@@ -30,5 +34,18 @@ defmodule J1605.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  defp description() do
+    "This mix is to be used to control J1605 switch hub, which contains 16 switches."
+  end
+
+  defp package() do
+    [
+      name: "j1605",
+      files: ~w(lib mix.exs README* readme* LICENSE* license*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/siegfried/j1605"}
+    ]
   end
 end
